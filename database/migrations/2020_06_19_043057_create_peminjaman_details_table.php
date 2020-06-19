333fa62282797512dabbf662e57945336e1e1a63
raisrindo <raisrindo@gmail.com>
2020-06-19 11:46:18 +0700

    menambah rancangan database beserta modelnya

--- database/migrations/2020_06_19_043057_create_peminjaman_details_table.php
+++ database/migrations/2020_06_19_043057_create_peminjaman_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePeminjamanDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('peminjaman_details', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('nomorinduk')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('ruangan_id');
+            $table->integer('peminjaman_id');
+            $table->date('tanggal_pakai');
+            $table->date('tanggal_selesaipakai');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('peminjaman_details');
     }
 }
