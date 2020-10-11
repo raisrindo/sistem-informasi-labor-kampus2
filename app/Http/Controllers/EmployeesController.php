6b0c8144178ba2dacd1cd07a2e9ad07b3d30d19b
Rais Rindo <muh.raisridho@gmail.com>
2020-10-11 11:52:16 +0700

    check point aplikasi v2.2

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -12,6 +12,12 @@ class EmployeesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role');
+    }
+    
     public function index()
     {
         $employees = Employee::all();
